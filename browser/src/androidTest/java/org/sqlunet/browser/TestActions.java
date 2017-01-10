2ce236d0895b9bc288605984794f1d3e585a2b9b
bbou <bbou@ac-toulouse.fr>
2017-01-10 08:19:25 +0100

    test

--- browser/src/androidTest/java/org/sqlunet/browser/TestActions.java
+++ browser/src/androidTest/java/org/sqlunet/browser/TestActions.java
@@ -1,8 +1,12 @@
 package org.sqlunet.browser;
 
 import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.contrib.DrawerActions;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Spinner;
+
+import org.hamcrest.Matcher;
 
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
@@ -11,10 +15,13 @@ import static android.support.test.espresso.action.ViewActions.pressImeActionBut
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static android.support.test.espresso.matcher.ViewMatchers.isDescendantOfA;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayingAtLeast;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.instanceOf;
@@ -38,16 +45,36 @@ public class TestActions
 
 	static void typeSearch(final String word, final int searchViewId)
 	{
+		final Matcher<View> searchView = withId(searchViewId);
+
 		// open search view
-		onView(withId(searchViewId)).check(matches(isDisplayed())).perform(click());
+		onView(searchView) //
+				.check(matches(isDisplayed())) //
+				.perform(click());
 
 		// type search
-		onView(allOf(isDisplayed(), isAssignableFrom(EditText.class))) //
+		onView(allOf(isDescendantOfA(searchView), isAssignableFrom(EditText.class))) //
 				.check(matches(isDisplayed())) //
 				.perform( //
 						typeText(word),  //
 						pressImeActionButton() //
 				);
+	}
+
+	static public void navigationDrawer(final String targetText)
+	{
+		int navigationDrawerId = R.id.navigation_drawer;
+
+		onView(withId(R.id.drawer_layout)) //
+				.perform(DrawerActions.open());
+
+		onView(allOf( //
+				withParent(withId(navigationDrawerId)), //
+				withText(targetText))) //
+				.perform(ViewActions.click());
+
+		onView(withId(R.id.drawer_layout)) //
+				.perform(DrawerActions.close());
 	}
 
 	static void swipeUp(final int viewId)
