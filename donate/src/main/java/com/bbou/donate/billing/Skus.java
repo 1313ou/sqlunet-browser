19d873f2262a35c09909be370881e43c6d8dccbc
bbou <bbou@ac-toulouse.fr>
2019-08-13 11:31:38 +0200

    wip

--- donate/src/main/java/com/bbou/donate/billing/Skus.java
+++ donate/src/main/java/com/bbou/donate/billing/Skus.java
@@ -13,38 +13,27 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.sqlunet.donate.billing;
+package com.bbou.donate.billing;
 
-import com.android.billingclient.api.BillingClient;
 import com.android.billingclient.api.BillingClient.SkuType;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * Static fields and methods useful for billing
  */
-public final class BillingConstants
+public final class Skus
 {
-	// SKUs for our products: the donate (consumable)
+	// SKUs for products: the donate (consumable)
 	static public final String SKU_DONATE1 = "donate_1"; //1
 	static public final String SKU_DONATE2 = "donate_2"; //5
 	static public final String SKU_DONATE3 = "donate_3"; //10
 	static public final String SKU_DONATE4 = "donate_5"; //20
 	static public final String SKU_DONATE5 = "donate_4"; //50
+	public static final String[] IN_APP_SKUS = {SKU_DONATE1, SKU_DONATE2, SKU_DONATE3, SKU_DONATE4, SKU_DONATE5};
 
-	// SKU for our subscription (infinite gas)
-
-	private static final String[] IN_APP_SKUS = {SKU_DONATE1, SKU_DONATE2, SKU_DONATE3, SKU_DONATE4, SKU_DONATE5};
-	private static final String[] SUBSCRIPTIONS_SKUS = {};
-
-	/**
-	 * Returns the list of all SKUs for the billing type specified
-	 */
-	public static List<String> getSkuList(@SkuType String billingType)
-	{
-		return SkuType.INAPP.equals(billingType) ? Arrays.asList(IN_APP_SKUS) : Arrays.asList(SUBSCRIPTIONS_SKUS);
-	}
+	// SKU for subscription
+	public static final String[] SUBSCRIPTIONS_SKUS = {};
 }
 
---
+++ donate/src/test/java/com/bbou/donate/BillingUnitTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2017 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.bbou.donate;
+
+import static org.junit.Assert.assertTrue;
+
+import com.android.billingclient.api.BillingClient;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.bbou.donate.billing.Skus;
+
+import static com.bbou.donate.billing.Skus.SKU_DONATE1;
+import static com.bbou.donate.billing.Skus.SKU_DONATE2;
+import static com.bbou.donate.billing.Skus.SKU_DONATE3;
+import static com.bbou.donate.billing.Skus.SKU_DONATE4;
+import static com.bbou.donate.billing.Skus.SKU_DONATE5;
+
+import org.junit.Test;
+
+/**
+ * Unit tests for billing model.
+ * <p>
+ * Note:
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ * <p>
+ * Testing Fundamentals:
+ * http://d.android.com/tools/testing/testing_android.html
+ * <p>
+ * Local Unit tests:
+ * https://d.android.com/training/testing/unit-testing/local-unit-tests.html
+ */
+public class BillingUnitTest
+{
+	@Test
+	public void billingHelperData_isConsistent() throws Exception
+	{
+		List<String> inAppList = Arrays.asList(Skus.IN_APP_SKUS);
+		assertTrue(inAppList.contains(SKU_DONATE1));
+		assertTrue(inAppList.contains(SKU_DONATE2));
+		assertTrue(inAppList.contains(SKU_DONATE3));
+		assertTrue(inAppList.contains(SKU_DONATE4));
+		assertTrue(inAppList.contains(SKU_DONATE5));
+		List<String> subscriptionsList = Arrays.asList(Skus.SUBSCRIPTIONS_SKUS);
+		assertTrue(subscriptionsList.isEmpty());
+	}
+}
