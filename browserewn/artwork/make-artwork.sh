15ff83f409b8e7fcdec0adca7d203d5cca126a00
bbou <bbou@ac-toulouse.fr>
2019-08-19 15:19:14 +0200

    logo_app

--- browserewn/artwork/make-artwork.sh
+++ browserewn/artwork/make-artwork.sh
@@ -4,11 +4,13 @@ source "../../make-artwork-lib.sh"
 
 launch_list="ic_launcher.svg ic_launcher_round.svg"
 logo_list="ic_logo.svg"
+logo_app_list="logo_app.svg"
 splash_list="ic_splash.svg"
 supersplash_list="sqlunet.svg home.svg"
 
 make_mipmap "${launch_list}" 48
 make_res "${logo_list}" 64
+make_res "${logo_app_list}" 48
 make_res "${splash_list}" 144
 make_res "${supersplash_list}" 400
 
