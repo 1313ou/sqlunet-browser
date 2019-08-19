43cbdc0f7e0adf2f10fac8c4259967fab40d84ee
bbou <bbou@ac-toulouse.fr>
2019-08-19 15:19:14 +0200

    logo_app

--- browservn/artwork/make-artwork.sh
+++ browservn/artwork/make-artwork.sh
@@ -4,6 +4,7 @@ source "../../make-artwork-lib.sh"
 
 launch_list="ic_launcher.svg ic_launcher_round.svg"
 logo_list="ic_logo.svg"
+logo_app_list="logo_app.svg"
 splash_list="ic_splash.svg"
 supersplash_list="sqlunet.svg home.svg"
 icon_list="ic_selector.svg ic_xselector.svg ic_role.svg ic_roles.svg ic_rows_bysynset.svg ic_rows_byrole.svg ic_roles_grouped.svg ic_rows_ungrouped.svg ic_search_wnword.svg ic_search_wndefinition.svg ic_search_wnsample.svg ic_search_vnexample.svg ic_search_pbexample.svg ic_search_fnsentence.svg ic_unknown.svg ic_ok.svg ic_fail.svg ic_setup.svg ic_download.svg ic_run.svg ic_download_source.svg ic_download_dest.svg"
@@ -15,6 +16,7 @@ domain_list="wordnet.svg verbnet.svg propbank.svg framenet.svg bnc.svg predicate
 
 make_mipmap "${launch_list}" 48
 make_res "${logo_list}" 64
+make_res "${logo_app_list}" 48
 make_res "${splash_list}" 144
 make_res "${supersplash_list}" 400
 make_res "${icon_list}" 32
