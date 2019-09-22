21817440605d9c938869dea5e69b99673713427a
bbou <bbou@ac-toulouse.fr>
2019-09-22 17:29:43 +0200

    wip

--- common/src/main/java/org/sqlunet/browser/Main2Activity.java
+++ common/src/main/java/org/sqlunet/browser/Main2Activity.java
@@ -4,66 +4,135 @@
 
 package org.sqlunet.browser;
 
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
-import android.view.View;
+import android.view.MenuItem;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.bbou.rate.AppRate;
 import com.google.android.material.navigation.NavigationView;
-import com.google.android.material.snackbar.Snackbar;
 
 import org.sqlunet.browser.common.R;
+import org.sqlunet.settings.StorageSettings;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class Main2Activity extends AppCompatActivity
 {
+	static private final String TAG = "MainA";
+
 	private AppBarConfiguration appBarConfiguration;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+
+		// rate
+		AppRate.invoke(this);
+
+		// info
+		Log.d(TAG, "DATABASE=" + StorageSettings.getDatabasePath(getBaseContext()));
+
+		// content view
 		setContentView(R.layout.activity_main2);
 
+		// toolbar
 		final Toolbar toolbar = findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 
-		/*
-		FloatingActionButton fab = findViewById(R.id.fab);
-		fab.setOnClickListener(new View.OnClickListener()
-		{
-			@Override
-			public void onClick(View view)
-			{
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-			}
-		});
-		*/
-
+		// navigation
 		final DrawerLayout drawer = findViewById(R.id.drawer_layout);
 		final NavigationView navigationView = findViewById(R.id.nav_view);
 		final NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
 
-		// Passing each menu ID as a set of Ids because each menu should be considered as top level destinations.
+		// passing each menu ID as a set of Ids because each menu should be considered as top level destinations.
 		this.appBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_home, R.id.nav_search_browse, R.id.nav_search_text, R.id.nav_status, R.id.nav_setup, R.id.nav_settings).setDrawerLayout(drawer).build();
 		NavigationUI.setupActionBarWithNavController(this, navController, this.appBarConfiguration);
 		NavigationUI.setupWithNavController(navigationView, navController);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
+	protected void onResume()
 	{
-		// Inflate the menu; this adds items to the action bar if it is present.
+		super.onResume();
+
+		// check hook
+		EntryActivity.forkOffIfCantRun(this);
+
+		// handle sent intent
+		handleSearchIntent(getIntent());
+	}
+
+	@Override
+	protected void onNewIntent(@NonNull final Intent intent)
+	{
+		super.onNewIntent(intent);
+		handleSearchIntent(intent);
+	}
+
+	// S E A R C H
+
+	/**
+	 * Handle intent dispatched by search view (either onCreate or onNewIntent if activity is single top)
+	 *
+	 * @param intent intent
+	 */
+	private void handleSearchIntent(@NonNull final Intent intent)
+	{
+		final NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+		final Fragment fragment = navHostFragment.getChildFragmentManager().getFragments().get(0);
+		if (fragment instanceof BaseSearchFragment)
+		{
+			final String action = intent.getAction();
+			final boolean isActionView = Intent.ACTION_VIEW.equals(action);
+			if (isActionView || Intent.ACTION_SEARCH.equals(action))
+			{
+				final BaseSearchFragment searchFragment = (BaseSearchFragment) fragment;
+				// search query submit (SEARCH) or suggestion selection (when a suggested item is selected) (VIEW)
+				final String query = intent.getStringExtra(SearchManager.QUERY);
+
+				if (isActionView)
+				{
+					searchFragment.clearQuery();
+				}
+
+				// search query submit or suggestion selection (when a suggested item is selected)
+				Log.d(TAG, "search " + query);
+				searchFragment.search(query);
+			}
+		}
+	}
+
+	// M E N U
+
+	@SuppressWarnings("SameReturnValue")
+	@Override
+	public boolean onCreateOptionsMenu(final Menu menu)
+	{
+		// inflate the menu; this adds items to the type bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+
+	@Override
+	public boolean onOptionsItemSelected(@NonNull final MenuItem item)
+	{
+		return MenuHandler.menuDispatch(this, item);
+	}
+
+	// N A V
 
 	@Override
 	public boolean onSupportNavigateUp()
