bebd8c17208fc4250d7de7b586ad089d5055d59b
bbou <bbou@ac-toulouse.fr>
2019-09-26 11:02:28 +0200

    preference summary providers

--- browser/src/main/java/org/sqlunet/browser/xn/DownloadPreferenceFragment.java
+++ browser/src/main/java/org/sqlunet/browser/xn/DownloadPreferenceFragment.java
@@ -6,8 +6,7 @@ package org.sqlunet.browser.xn;
 
 import android.os.Bundle;
 
-import org.sqlunet.browser.config.SettingsActivity;
-
+import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 
 /**
@@ -17,13 +16,12 @@ import androidx.preference.Preference;
  */
 public class DownloadPreferenceFragment extends org.sqlunet.browser.config.SettingsActivity.DownloadPreferenceFragment
 {
-
 	@Override
 	public void onCreatePreferences(final Bundle savedInstanceState, final String rootKey)
 	{
 		super.onCreatePreferences(savedInstanceState, rootKey);
-		final Preference preference = findPreference(Settings.PREF_ENTRY_PM);
-		assert preference != null;
-		SettingsActivity.bind(preference);
+		final Preference entryPmPreference = findPreference(Settings.PREF_ENTRY_PM);
+		assert entryPmPreference != null;
+		entryPmPreference.setSummaryProvider(ListPreference.SimpleSummaryProvider.getInstance());
 	}
 }
