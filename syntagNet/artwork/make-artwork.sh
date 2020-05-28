3794317a6f79d8b26986f4ef8f2723c1d8f76f50
1313ou <1313ou@gmail.com>
2020-05-28 09:19:53 +0200

    artwork

--- syntagNet/artwork/make-artwork.sh
+++ syntagNet/artwork/make-artwork.sh
@@ -3,7 +3,7 @@
 source "./lib-artwork.sh"
 
 icon_list="collocation.svg before.svg after.svg definition1.svg definition2.svg"
-asset_list=""
+asset_list="collocation.svg"
 
 make_res "${icon_list}" 16
 
