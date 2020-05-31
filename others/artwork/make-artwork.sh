e875a0659a62e410cdb6862bc12ffa289c972a04
1313ou <1313ou@gmail.com>
2020-05-31 16:14:17 +0200

    new artwork

--- others/artwork/make-artwork.sh
+++ others/artwork/make-artwork.sh
@@ -2,7 +2,7 @@
 
 source "./lib-artwork.sh"
 
-logo_list="logo_semantikos.svg logo_semantikos_wn.svg logo_semantikos_ewn.svg logo_semantikos_fn.svg logo_semantikos_vn.svg logo_treebolicwordnet.svg logo_grammarscope.svg logo_grammarscope_udpipe.svg"
+logo_list="logo_semantikos.svg logo_semantikos_wn.svg logo_semantikos_ewn.svg logo_semantikos_fn.svg logo_semantikos_vn.svg logo_semantikos_sn.svg logo_treebolicwordnet.svg logo_grammarscope.svg logo_grammarscope_udpipe.svg"
 googleplay_list="ic_googleplay.svg"
 
 make_res "${logo_list}" 48
