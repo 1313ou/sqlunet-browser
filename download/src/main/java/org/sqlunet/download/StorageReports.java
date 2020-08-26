2c778bfedb23272807e17b7f19e3805097d36eb5
1313ou <1313ou@gmail.com>
2020-08-26 08:01:50 +0200

    deprecations

--- download/src/main/java/org/sqlunet/download/StorageReports.java
+++ download/src/main/java/org/sqlunet/download/StorageReports.java
@@ -24,6 +24,7 @@ class StorageReports
 	 * @param context context
 	 * @return pair of names and values
 	 */
+	@SuppressWarnings("deprecation")
 	@NonNull
 	static public Pair<CharSequence[], CharSequence[]> getStyledCachesNamesValues(@NonNull final Context context)
 	{
