7cb201a14c2285bc906aa6d1fd6da259dcfaf4cd
1313ou <1313ou@gmail.com>
2020-12-28 16:03:31 +0100

    asset packs class

---
+++ browserfn/src/main/java/org/sqlunet/browser/fn/Assets.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020. Bernard Bou <1313ou@gmail.com>.
+ */
+
+package org.sqlunet.browser.fn;
+
+public class Assets
+{
+	public static final String WN31_PACKNAME = "dbfn_wn31_asset";
+
+	public static final String EWN_PACKNAME = "dbfn_ewn_asset";
+}
---
+++ browsersn/src/main/java/org/sqlunet/browser/sn/Assets.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020. Bernard Bou <1313ou@gmail.com>.
+ */
+
+package org.sqlunet.browser.sn;
+
+public class Assets
+{
+	public static final String WN31_PACKNAME = "dbsn_wn31_asset";
+
+	public static final String EWN_PACKNAME = "dbsn_ewn_asset";
+}
