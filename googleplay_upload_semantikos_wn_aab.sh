6e4129da4fb419fc0ebeb79156795732d442ccc6
1313ou <1313ou@gmail.com>
2021-01-04 09:59:36 +0100

    upload scripts

--- googleplay_upload_semantikos_wn_aab.sh
+++ googleplay_upload_semantikos_wn_aab.sh
@@ -2,9 +2,11 @@
 
 RELEASE_NAME="$1"
 if [ -z "${RELEASE_NAME}" ]; then
-	RELEASE_NAME="Anew"
+	V=`./find-version.sh`
+	RELEASE_NAME="I${V}"
 	echo "Version name ${RELEASE_NAME}"
 fi
+
 RECENT_CHANGES="$2"
 if [ -z "${RECENT_CHANGES}" ]; then
 	RECENT_CHANGES="Fixes"
