d767972d1579895b47615ca5102fcc2fe12b417e
1313ou <1313ou@gmail.com>
2021-01-04 09:59:36 +0100

    upload scripts

---
+++ find-version.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+thisdir="`dirname $(readlink -m $0)`"
+thisdir="$(readlink -m ${thisdir})"
+
+RED='\u001b[31m'
+GREEN='\u001b[32m'
+YELLOW='\u001b[33m'
+BLUE='\u001b[34m'
+MAGENTA='\u001b[35m'
+CYAN='\u001b[36m'
+RESET='\u001b[0m'
+
+grep 'versionCode = ' build.gradle | \
+sed -r  's/^[^0-9]*([0-9]+).*/\1/'
+
