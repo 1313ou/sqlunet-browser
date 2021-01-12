40faadfcb8b93d1b940a2c363ec85fe03a49f476
1313ou <1313ou@gmail.com>
2021-01-12 16:12:21 +0100

    global upload script

--- googleplay_upload_all_aabs.sh
+++ googleplay_upload_all_aabs.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-RELEASE_NAME="Anew"
-if [ ! -z "$1" ]; then
-	RELEASE_NAME="$1"
+RELEASE_NAME="$1"
+if [ -z "${RELEASE_NAME}" ]; then
+	V=`./find-version.sh`
+	RELEASE_NAME="I${V}"
 	echo "Version name ${RELEASE_NAME}"
 fi
 RECENT_CHANGES="Fixes"
 
-./googleplay_upload_semantikos_aab.sh		  "${RELEASE_NAME}" "${RECENT_CHANGES}"
+./googleplay_upload_semantikos_aab.sh		"${RELEASE_NAME}" "${RECENT_CHANGES}"
 ./googleplay_upload_semantikos_wn_aab.sh	"${RELEASE_NAME}" "${RECENT_CHANGES}"
 ./googleplay_upload_semantikos_ewn_aab.sh	"${RELEASE_NAME}" "${RECENT_CHANGES}"
 ./googleplay_upload_semantikos_fn_aab.sh	"${RELEASE_NAME}" "${RECENT_CHANGES}"
