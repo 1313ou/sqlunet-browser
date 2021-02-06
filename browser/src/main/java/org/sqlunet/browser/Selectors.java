4b1262e7cddd2de4856cde87f532835ea6d1f9d2
1313ou <1313ou@gmail.com>
2021-02-06 19:49:05 +0100

    define is_two_pane arg

---
+++ browser/src/main/java/org/sqlunet/browser/Selectors.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2020. Bernard Bou <1313ou@gmail.com>.
+ */
+
+package org.sqlunet.browser;
+
+public class Selectors
+{
+	/**
+	 * Argument to selectors fragment.
+	 */
+	public static final String IS_TWO_PANE = "is_two_pane";
+}
