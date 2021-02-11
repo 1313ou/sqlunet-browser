2494250d09a3bb7447bbc89b1a146e1614198551
1313ou <1313ou@gmail.com>
2021-02-11 07:45:48 +0100

    revise script to be more selective of line

--- apk-version.sh
+++ apk-version.sh
@@ -6,21 +6,24 @@ else
 	all=dist/releases/*.apk
 fi
 
-RED='\u001b[31m'
-GREEN='\u001b[32m'
-YELLOW='\u001b[33m'
-BLUE='\u001b[34m'
-MAGENTA='\u001b[35m'
-CYAN='\u001b[36m'
-RESET='\u001b[0m'
+R='\u001b[31m'
+G='\u001b[32m'
+Y='\u001b[33m'
+B='\u001b[34m'
+M='\u001b[35m'
+C='\u001b[36m'
+Z='\u001b[0m'
 
 for apk in ${all}; do
-	echo -e "${MAGENTA}${apk}${RESET}"
+	echo -e "${M}${apk}${Z}"
 	aapt l -a ${apk} | egrep '(package=|android:versionCode|android:versionName)'
 done
 
 for apk in ${all}; do
-	echo -en "${MAGENTA}`basename ${apk}`${RESET} "
-	n=`aapt l -a ${apk} | egrep versionCode | sed 's/^.*(type 0x10)/ /g'`
-	echo -e "${BLUE}$(($n))${RESET}" 
+	echo -en "${M}`basename ${apk}`${Z} "
+	#echo -e "${G}" 
+	#aapt l -a ${apk} | egrep 'android:versionCode' | sed 's/^.*(type 0x10)/ /g'
+	#echo -e "${Z}" 
+	n=`aapt l -a ${apk} | egrep 'android:versionCode' | sed 's/^.*(type 0x10)/ /g'`
+	echo -e "${B}$(($n))${Z}" 
 done
