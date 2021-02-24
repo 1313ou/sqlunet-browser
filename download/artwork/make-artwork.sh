fc8af0faf779e9d2e8674788905d3aed73be6490
1313ou <1313ou@gmail.com>
2021-02-24 11:16:06 +0100

    trim drawables

--- download/artwork/make-artwork.sh
+++ download/artwork/make-artwork.sh
@@ -2,12 +2,12 @@
 
 source "./lib-artwork.sh"
 
-icon_list="ic_ok.svg ic_download.svg ic_run.svg ic_download_source.svg ic_download_zip_source.svg ic_download_dest.svg ic_download.svg"
-smallicon_list="ic_error.svg"
+icon_list="ic_download.svg ic_download_source.svg ic_download_zip_source.svg ic_download_dest.svg ic_download.svg"
+notificon_list="ic_notif_cancel.svg"
 middleicon_list="bn_download.svg bn_download_ok.svg"
 
 make_res "${icon_list}" 32
-make_res "${smallicon_list}" 16
+make_res "${notificon_list}" 32
 make_res "${middleicon_list}" 24
 
 check
