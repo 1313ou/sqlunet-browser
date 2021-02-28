08208d474e28984e312de4a7d5980afa140cc280
1313ou <1313ou@gmail.com>
2021-02-28 18:49:18 +0100

    remove duplicates

--- browser/artwork/make-artwork.sh
+++ browser/artwork/make-artwork.sh
@@ -7,11 +7,8 @@ logo_list="ic_logo.svg"
 logo_app_list="logo_app.svg"
 splash_list="ic_splash.svg"
 supersplash_list="sqlunet.svg"
-icon_list="ic_selector.svg ic_xselector.svg ic_role.svg ic_roles.svg ic_rows_bysynset.svg ic_rows_byrole.svg ic_roles_grouped.svg ic_rows_ungrouped.svg ic_search_wnword.svg ic_search_wndefinition.svg ic_search_wnsample.svg ic_search_vnexample.svg ic_search_pbexample.svg ic_search_fnsentence.svg ic_unknown.svg ic_ok.svg ic_fail.svg ic_setup.svg ic_run.svg"
-smallicon_list="ic_item.svg"
-middleicon_list="bn_setup.svg bn_download.svg bn_info.svg"
-drawericon_list="ic_home.svg ic_search_browse.svg ic_search_text.svg ic_search_pm.svg ic_setup.svg ic_settings.svg ic_storage.svg ic_status.svg ic_sql.svg ic_help.svg"
-arrow_list="ic_spinner_arrow.svg"
+icon_list="ic_xselector.svg ic_rows_bysynset.svg ic_rows_byrole.svg ic_roles_grouped.svg ic_rows_ungrouped.svg ic_search_wnword.svg ic_search_wndefinition.svg ic_search_wnsample.svg ic_search_vnexample.svg ic_search_pbexample.svg ic_search_fnsentence.svg"
+drawericon_list="ic_search_pm.svg"
 
 make_mipmap "${launch_list}" 48
 make_res "${logo_list}" 64
@@ -19,10 +16,7 @@ make_res "${logo_app_list}" 48
 make_res "${splash_list}" 144
 make_res "${supersplash_list}" 400
 make_res "${icon_list}" 32
-make_res "${smallicon_list}" 16
-make_res "${middleicon_list}" 24
 make_res "${drawericon_list}" 32
-make_res "${arrow_list}" 10
 
 make_help "logo.svg" 128
 make_help "sqlunet.svg" 384
