8abae621dde66a45fe489102e36b13fd7cc5a525
1313ou <1313ou@gmail.com>
2021-02-28 11:30:42 +0100

    lint unused

--- browsersn/artwork/make-artwork.sh
+++ browsersn/artwork/make-artwork.sh
@@ -10,7 +10,7 @@ supersplash_list="home.svg"
 icon_list="ic_selector.svg ic_xselector.svg ic_snselector.svg ic_search_wnword.svg ic_search_wndefinition.svg ic_unknown.svg ic_ok.svg ic_fail.svg ic_setup.svg ic_run.svg ic_download_source.svg ic_download_dest.svg"
 smallicon_list="ic_item.svg ic_collocation.svg"
 middleicon_list="bn_setup.svg bn_download.svg bn_info.svg"
-drawericon_list="ic_home.svg ic_search_browse.svg ic_search_text.svg ic_setup.svg ic_settings.svg ic_storage.svg ic_status.svg ic_sql.svg ic_help.svg ic_about.svg"
+drawericon_list="ic_home.svg ic_search_browse.svg ic_search_text.svg ic_setup.svg ic_settings.svg ic_storage.svg ic_status.svg ic_sql.svg ic_help.svg"
 arrow_list="ic_spinner_arrow.svg"
 
 make_mipmap "${launch_list}" 48
