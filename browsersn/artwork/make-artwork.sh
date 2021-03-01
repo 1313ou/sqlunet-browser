a4ab9c3947c7d7834af3f54758e5b5ae78194459
1313ou <1313ou@gmail.com>
2021-03-01 08:07:45 +0100

    clean up

--- browsersn/artwork/make-artwork.sh
+++ browsersn/artwork/make-artwork.sh
@@ -6,9 +6,8 @@ launch_list="ic_launcher.svg ic_launcher_round.svg"
 logo_list="ic_logo.svg"
 splash_list="ic_splash.svg"
 supersplash_list="home.svg"
-icon_list="ic_xselector.svg ic_snselector.svg ic_search_wnword.svg ic_search_wndefinition.svg ic_unknown.svg ic_ok.svg ic_fail.svg"
-smallicon_list="ic_item.svg ic_collocation.svg"
-middleicon_list="bn_setup.svg bn_download.svg bn_info.svg"
+icon_list="ic_xselector.svg ic_snselector.svg ic_search_wnword.svg ic_search_wndefinition.svg"
+smallicon_list="ic_collocation.svg"
 
 make_mipmap "${launch_list}" 48
 make_res "${logo_list}" 64
@@ -16,7 +15,6 @@ make_res "${splash_list}" 144
 make_res "${supersplash_list}" 400
 make_res "${icon_list}" 32
 make_res "${smallicon_list}" 16
-make_res "${middleicon_list}" 24
 
 make_help "logo.svg" 128
 make_help "sqlunet.svg" 384
