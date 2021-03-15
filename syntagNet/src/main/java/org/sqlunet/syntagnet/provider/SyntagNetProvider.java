ce2513dc93b1a168d02c378258f59e58f263e7b8
1313ou <1313ou@gmail.com>
2021-03-15 09:11:08 +0100

    lint

--- syntagNet/src/main/java/org/sqlunet/syntagnet/provider/SyntagNetProvider.java
+++ syntagNet/src/main/java/org/sqlunet/syntagnet/provider/SyntagNetProvider.java
@@ -165,7 +165,6 @@ public class SyntagNetProvider extends BaseProvider
 				throw new RuntimeException("Malformed URI " + uri);
 		}
 
-		@SuppressWarnings("ConstantConditions")
 		final String sql = SQLiteQueryBuilder.buildQueryString(false, table, projection, actualSelection, groupBy, null, actualSortOrder, null);
 		logSql(sql, selectionArgs);
 		if (BaseProvider.logSql)
