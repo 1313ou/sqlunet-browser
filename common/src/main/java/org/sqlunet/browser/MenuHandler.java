4bf8b9b4b377be051f17132735282e173efe3c27
1313ou <1313ou@gmail.com>
2021-03-16 16:55:57 +0100

    new start signature with download intent common menu handling factored out

--- common/src/main/java/org/sqlunet/browser/MenuHandler.java
+++ common/src/main/java/org/sqlunet/browser/MenuHandler.java
@@ -19,6 +19,7 @@ import com.bbou.rate.AppRate;
 
 import org.sqlunet.browser.common.R;
 import org.sqlunet.browser.config.DiagnosticsActivity;
+import org.sqlunet.browser.config.DownloadActivity;
 import org.sqlunet.browser.config.SettingsActivity;
 import org.sqlunet.browser.config.SetupActivity;
 import org.sqlunet.browser.config.SetupAsset;
@@ -27,7 +28,6 @@ import org.sqlunet.browser.config.SetupFileFragment;
 import org.sqlunet.browser.config.StorageActivity;
 import org.sqlunet.concurrency.TaskDialogObserver;
 import org.sqlunet.concurrency.TaskObserver;
-import org.sqlunet.download.DownloadActivity;
 import org.sqlunet.download.FileDataDownloader;
 import org.sqlunet.provider.BaseProvider;
 import org.sqlunet.settings.Settings;
@@ -78,14 +78,6 @@ public class MenuHandler
 		{
 			intent = new Intent(activity, StatusActivity.class);
 		}
-		else if (itemId == R.id.action_storage)
-		{
-			intent = new Intent(activity, StorageActivity.class);
-		}
-		else if (itemId == R.id.action_diagnostics)
-		{
-			intent = new Intent(activity, DiagnosticsActivity.class);
-		}
 		else if (itemId == R.id.action_provider_info)
 		{
 			Providers.listProviders(activity);
@@ -93,11 +85,6 @@ public class MenuHandler
 		}
 
 		// change data
-		else if (itemId == R.id.action_drop)
-		{
-			intent = new Intent(activity, SetupFileActivity.class);
-			intent.putExtra(SetupFileFragment.ARG, SetupFileFragment.Operation.DROP.toString());
-		}
 		else if (itemId == R.id.action_setup)
 		{
 			intent = new Intent(activity, SetupActivity.class);
@@ -115,65 +102,9 @@ public class MenuHandler
 			final String downloadSourceUrl = StorageSettings.getDbDownloadSource(activity);
 			final String downloadDest = StorageSettings.getDatabasePath(activity);
 			final String cache = StorageSettings.getCacheDir(activity);
-			FileDataDownloader.start(activity, name, downloadSourceUrl, downloadDest, cache);
-			return true;
-		}
-		else if (itemId == R.id.action_asset_deliver)
-		{
-			final String asset = Settings.getAssetPack(activity);
-			final String assetDir = Settings.getAssetPackDir(activity);
-			final String assetZip = Settings.getAssetPackZip(activity);
-			final String assetZipEntry = activity.getString(R.string.asset_zip_entry);
-			final TaskObserver.Observer<Number> observer = new TaskDialogObserver<>(activity.getSupportFragmentManager()) //
-					.setTitle(activity.getString(R.string.title_dialog_assetload)) //
-					.setMessage(asset);
-			SetupAsset.deliverAsset(asset, assetDir, assetZip, assetZipEntry, activity, observer, null, null);
-			return true;
-		}
-		else if (itemId == R.id.action_asset_deliver_primary)
-		{
-			final String asset = activity.getString(R.string.asset_primary);
-			final String assetDir = activity.getString(R.string.asset_dir_primary);
-			final String assetZip = activity.getString(R.string.asset_zip_primary);
-			final String assetZipEntry = activity.getString(R.string.asset_zip_entry);
-			final TaskObserver.Observer<Number> observer = new TaskDialogObserver<>(activity.getSupportFragmentManager()) //
-					.setTitle(activity.getString(R.string.title_dialog_assetload)) //
-					.setMessage(asset);
-			SetupAsset.deliverAsset(asset, assetDir, assetZip, assetZipEntry, activity, observer, null, null);
-			return true;
-		}
-		else if (itemId == R.id.action_asset_deliver_alt)
-		{
-			final String asset = activity.getString(R.string.asset_alt);
-			final String assetDir = activity.getString(R.string.asset_dir_alt);
-			final String assetZip = activity.getString(R.string.asset_zip_alt);
-			final String assetZipEntry = activity.getString(R.string.asset_zip_entry);
-			if (!asset.isEmpty())
-			{
-				final TaskObserver.Observer<Number> observer = new TaskDialogObserver<>(activity.getSupportFragmentManager()) //
-						.setTitle(activity.getString(R.string.title_dialog_assetload)) //
-						.setMessage(asset);
-				SetupAsset.deliverAsset(asset, assetDir, assetZip, assetZipEntry, activity, observer, null, null);
-			}
-			return true;
-		}
-		else if (itemId == R.id.action_asset_dispose)
-		{
-			SetupAsset.disposeAsset(Settings.getAssetPack(activity), activity);
-			return true;
-		}
-		else if (itemId == R.id.action_asset_dispose_primary)
-		{
-			SetupAsset.disposeAsset(activity.getString(R.string.asset_primary), activity);
-			return true;
-		}
-		else if (itemId == R.id.action_asset_dispose_alt)
-		{
-			String asset = activity.getString(R.string.asset_alt);
-			if (!asset.isEmpty())
-			{
-				SetupAsset.disposeAsset(asset, activity);
-			}
+			//final Intent downloadIntent = new Intent(activity, DownloadActivity.class);
+			final Intent downloadIntent = null;
+			FileDataDownloader.start(activity, name, downloadSourceUrl, downloadDest, downloadIntent, cache);
 			return true;
 		}
 
@@ -239,37 +170,9 @@ public class MenuHandler
 			AppRate.rate(activity);
 			return true;
 		}
-		else if (itemId == R.id.action_quit)
-		{
-			activity.finish();
-			return true;
-		}
-		else if (itemId == R.id.action_appsettings)
-		{
-			final String appId = activity.getPackageName();
-			Settings.applicationSettings(activity, appId);
-			return true;
-		}
-		else if (itemId == R.id.action_theme_system)
-		{
-			AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
-			return true;
-		}
-		else if (itemId == R.id.action_theme_night)
-		{
-			AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-			Log.d("MenuHandler", "set night mode from " + activity.getComponentName());
-			return true;
-		}
-		else if (itemId == R.id.action_theme_day)
-		{
-			AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-			Log.d("MenuHandler", "set day mode from " + activity.getComponentName());
-			return true;
-		}
 		else
 		{
-			return false;
+			return menuDispatchCommon(activity, itemId);
 		}
 
 		// start activity
@@ -297,14 +200,6 @@ public class MenuHandler
 			intent = new Intent(activity, StatusActivity.class);
 			intent.addFlags(EntryActivity.INITIAL_TASK_FLAGS);
 		}
-		else if (itemId == R.id.action_storage)
-		{
-			intent = new Intent(activity, StorageActivity.class);
-		}
-		else if (itemId == R.id.action_diagnostics)
-		{
-			intent = new Intent(activity, DiagnosticsActivity.class);
-		}
 
 		// change data
 		else if (itemId == R.id.action_setup)
@@ -314,20 +209,64 @@ public class MenuHandler
 		}
 		else if (itemId == R.id.action_download)
 		{
-			intent = new Intent(activity, org.sqlunet.browser.config.DownloadActivity.class);
+			intent = new Intent(activity, DownloadActivity.class);
 			intent.putExtra(DOWNLOAD_FROM_ARG, StorageSettings.getDbDownloadSource(activity));
 			intent.putExtra(DOWNLOAD_TO_ARG, StorageSettings.getDbDownloadTarget(activity));
 			intent.addFlags(EntryActivity.INITIAL_TASK_FLAGS);
 		}
-		else if (itemId == R.id.action_update)
+
+		// settings
+		else if (itemId == R.id.action_download_settings)
 		{
-			BaseProvider.closeProviders(activity);
-			final String name = activity.getResources().getString(R.string.pref_default_download_dbfile);
-			final String downloadSourceUrl = StorageSettings.getDbDownloadSource(activity);
-			final String downloadDest = StorageSettings.getDatabasePath(activity);
-			final String cache = StorageSettings.getCacheDir(activity);
-			FileDataDownloader.start(activity, name, downloadSourceUrl, downloadDest, cache);
-			return true;
+			intent = new Intent(activity, SettingsActivity.class);
+			intent.putExtra(INITIAL_ARG, true);
+			intent.addFlags(EntryActivity.INITIAL_TASK_FLAGS);
+		}
+		else if (itemId == R.id.action_clear_settings)
+		{
+			final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(activity);
+			final SharedPreferences.Editor edit = prefs.edit();
+			edit.clear().apply();
+			intent = new Intent(activity, SettingsActivity.class);
+			intent.addFlags(EntryActivity.INITIAL_TASK_FLAGS);
+		}
+
+		// others
+		else
+		{
+			return menuDispatchCommon(activity, itemId);
+		}
+
+		// start activity
+		activity.startActivity(intent);
+		return true;
+	}
+
+	/**
+	 * Dispatch menu item action, common
+	 *
+	 * @param activity activity
+	 * @param itemId   menu item id
+	 * @return true if processed/consumed
+	 */
+	static private boolean menuDispatchCommon(@NonNull final AppCompatActivity activity, final int itemId)
+	{
+		Intent intent;
+
+		if (itemId == R.id.action_storage)
+		{
+			intent = new Intent(activity, StorageActivity.class);
+		}
+		else if (itemId == R.id.action_diagnostics)
+		{
+			intent = new Intent(activity, DiagnosticsActivity.class);
+		}
+
+		// change data
+		else if (itemId == R.id.action_drop)
+		{
+			intent = new Intent(activity, SetupFileActivity.class);
+			intent.putExtra(SetupFileFragment.ARG, SetupFileFragment.Operation.DROP.toString());
 		}
 		else if (itemId == R.id.action_asset_deliver)
 		{
@@ -387,34 +326,8 @@ public class MenuHandler
 			}
 			return true;
 		}
-		else if (itemId == R.id.action_drop)
-		{
-			intent = new Intent(activity, SetupFileActivity.class);
-			intent.putExtra(SetupFileFragment.ARG, SetupFileFragment.Operation.DROP.toString());
-		}
-
-		// settings
-		else if (itemId == R.id.action_download_settings)
-		{
-			intent = new Intent(activity, SettingsActivity.class);
-			intent.putExtra(INITIAL_ARG, true);
-			intent.addFlags(EntryActivity.INITIAL_TASK_FLAGS);
-		}
-		else if (itemId == R.id.action_clear_settings)
-		{
-			final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(activity);
-			final SharedPreferences.Editor edit = prefs.edit();
-			edit.clear().apply();
-			intent = new Intent(activity, SettingsActivity.class);
-			intent.addFlags(EntryActivity.INITIAL_TASK_FLAGS);
-		}
 
 		// others
-		else if (itemId == R.id.action_quit)
-		{
-			activity.finish();
-			return true;
-		}
 		else if (itemId == R.id.action_appsettings)
 		{
 			final String appId = activity.getPackageName();
@@ -436,6 +349,11 @@ public class MenuHandler
 		{
 			AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 			Log.d("MenuHandler", "set day mode from " + activity.getComponentName());
+			return true;
+		}
+		else if (itemId == R.id.action_quit)
+		{
+			activity.finish();
 			return true;
 		}
 		else
