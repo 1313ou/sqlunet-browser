4f0d029f381f609b973e7acfc720792b9f9db249
1313ou <1313ou@gmail.com>
2022-03-14 19:48:31 +0100

    update to sqlunet2 names

--- wordNet/src/main/java/org/sqlunet/wordnet/sql/SqLiteDialect.java
+++ wordNet/src/main/java/org/sqlunet/wordnet/sql/SqLiteDialect.java
@@ -11,55 +11,72 @@ package org.sqlunet.wordnet.sql;
  */
 class SqLiteDialect
 {
-	// query for lexical domains enumeration
-	static final String AllDomainsQuery = "SELECT domainid, domainname, pos " +  //
+	// query for domains enumeration
+	static final String AllDomainsQuery = //
+			"SELECT domainid, domain, posid " +  //
 			"FROM domains " +  //
 			"ORDER BY domainid;";
+
 	// query for relations enumeration
-	static final String AllRelationsQuery = "SELECT relationid, relation, recurses " +  //
+	static final String AllRelationsQuery = //
+			"SELECT relationid, relation, recurses " +  //
 			"FROM relations " +  //
 			"ORDER BY relationid;";
 
 	// WORD
+
 	// query for word id
-	static final String WordQuery = "SELECT wordid, lemma " +  //
+	static final String WordQuery = //
+			"SELECT wordid, word " +  //
 			"FROM words " +  //
 			"WHERE wordid = ?;";
+
 	// query for word
-	static final String WordQueryFromLemma = "SELECT wordid, lemma " +  //
+	static final String WordQueryFromLemma = //
+			"SELECT wordid, word " +  //
 			"FROM words " +  //
-			"WHERE lemma = ?;";
+			"WHERE word = ?;";
 
 	// SYNSET
+
 	// query for synset from synset id
-	static final String SynsetQuery = "SELECT synsetid, definition, domainid, GROUP_CONCAT(sample, '|' ) AS sampleset " +  //
+	static final String SynsetQuery = //
+			"SELECT synsetid, definition, domainid, GROUP_CONCAT(sample, '|' ) AS sampleset " +  //
 			"FROM synsets " +  //
 			"LEFT JOIN samples USING (synsetid) " +  //
 			"WHERE synsetid = ? " +  //
 			"GROUP BY synsetid;";
+
 	// query for words in synsets
-	static final String SynsetWordsQuery = "SELECT lemma, wordid " +  //
+	static final String SynsetWordsQuery = //
+			"SELECT word, wordid " +  //
 			"FROM senses " +  //
 			"INNER JOIN words USING (wordid) " + //
 			"WHERE synsetid = ?;";
+
 	// query for synsets from word id
-	static final String SynsetsQueryFromWordId = "SELECT synsetid, definition, pos, domainid, GROUP_CONCAT(sample, '|' ) AS sampleset " +  //
+	static final String SynsetsQueryFromWordId = //
+			"SELECT synsetid, definition, posid, domainid, GROUP_CONCAT(sample, '|' ) AS sampleset " +  //
 			"FROM senses " +  //
 			"INNER JOIN synsets USING (synsetid) " + //
 			"LEFT JOIN samples USING (synsetid) " + //
 			"WHERE wordid = ? " + //
 			"GROUP BY synsetid " +  //
 			"ORDER BY domainid ASC, sensenum DESC;";
-	// query for synsets of given pos type from word id
-	static final String SynsetsQueryFromWordIdAndPos = "SELECT synsetid, definition, domainid, GROUP_CONCAT(sample, '|' ) AS sampleset " + //
+
+	// query for synsets of given pos id from word id
+	static final String SynsetsQueryFromWordIdAndPos = //
+			"SELECT synsetid, definition, domainid, GROUP_CONCAT(sample, '|' ) AS sampleset " + //
 			"FROM senses " + //
 			"INNER JOIN synsets USING (synsetid) " + //
-			"LEFT JOIN samples USING (synsetid) " + //
-			"WHERE wordid = ? AND pos = ? " + //
+			"LEFT JOIN samples USING (synsets.synsetid) " + //
+			"WHERE wordid = ? AND posid = ? " + //
 			"GROUP BY synsetid " + //
 			"ORDER BY domainid ASC, sensenum ASC;";
+
 	// query for synsets of given domain from word
-	static final String SynsetsQueryFromWordIdAndDomainId = "SELECT synsetid, definition, domainid, GROUP_CONCAT(sample, '|' ) AS sampleset " + //
+	static final String SynsetsQueryFromWordIdAndDomainId = //
+			"SELECT synsetid, definition, domainid, GROUP_CONCAT(sample, '|' ) AS sampleset " + //
 			"FROM senses " + //
 			"INNER JOIN synsets USING (synsetid) " + //
 			"LEFT JOIN samples USING (synsetid) " + //
@@ -69,15 +86,16 @@ class SqLiteDialect
 
 	// RELATIONS
 	// query for relateds from synsets
-	static final String RelatedsQueryFromSynsetId = "SELECT relationid, synsetid, definition, domainid, GROUP_CONCAT(sample, '|' ) AS sampleset, 0 AS word2id, NULL AS word2, synset1id, 0 " +  //
-			"FROM semrelations " + //
+	static final String RelatedsQueryFromSynsetId = //
+			"SELECT relationid, synsetid, definition, domainid, GROUP_CONCAT(sample, '|' ) AS sampleset, 0 AS word2id, NULL AS word2, synset1id, 0 " +  //
+			"FROM synsets_synsets " + //
 			"INNER JOIN synsets ON synset2id = synsetid " +  //
 			"LEFT JOIN relations USING (relationid) " + //
 			"LEFT JOIN samples USING (synsetid) " + //
 			"WHERE synset1id = ? " + //
 			"GROUP BY synsetid " + //
 			"UNION " + //
-			"SELECT relationid, synsetid, definition, domainid, GROUP_CONCAT(sample, '|' ) AS sampleset, GROUP_CONCAT(DISTINCT word2id), GROUP_CONCAT(DISTINCT lemma) AS word2, synset1id, word1id " + //
+			"SELECT relationid, synsetid, definition, domainid, GROUP_CONCAT(sample, '|' ) AS sampleset, GROUP_CONCAT(DISTINCT word2id), GROUP_CONCAT(DISTINCT word) AS word2, synset1id, word1id " + ///
 			"FROM senses_senses " + //
 			"INNER JOIN synsets ON synset2id = synsetid " + //
 			"LEFT JOIN words ON word2id = wordid " + //
@@ -86,8 +104,10 @@ class SqLiteDialect
 			"WHERE synset1id = ? AND CASE ? WHEN 0 THEN 1 ELSE word1id = ? END " + //
 			"GROUP BY synsetid " + //
 			"ORDER BY 1, 2;";
+
 	// query for relateds from synsets and relation type
-	static final String RelatedsQueryFromSynsetIdAndRelationId = "SELECT relationid, synsetid, definition, domainid, GROUP_CONCAT(sample, '|' ) AS samples, 0 AS word2id, NULL AS word2, synset1id, 0 " + //
+	static final String RelatedsQueryFromSynsetIdAndRelationId = //
+			"SELECT relationid, synsetid, definition, domainid, GROUP_CONCAT(sample, '|' ) AS sampleset, 0 AS word2id, NULL AS word2, synset1id, 0 " + //
 			"FROM synsets_synsets " + //
 			"INNER JOIN synsets ON synset2id = synsetid " + //
 			"LEFT JOIN relations USING (relationid) " + //
@@ -95,8 +115,8 @@ class SqLiteDialect
 			"WHERE synset1id = ? AND relationid = ? " + //
 			"GROUP BY synsetid " + //
 			"UNION " + //
-			"SELECT relationid, synsetid, definition, domainid, GROUP_CONCAT(sample, '|' ) AS samples, GROUP_CONCAT(DISTINCT word2id), GROUP_CONCAT(DISTINCT lemma) AS word2, synset1id, word1id " + //
-			"FROM synsets_synsets " + //
+			"SELECT relationid, synsetid, definition, domainid, GROUP_CONCAT(sample, '|' ) AS sampleset, GROUP_CONCAT(DISTINCT word2id), GROUP_CONCAT(DISTINCT word) AS word2, synset1id, word1id " + //
+			"FROM senses_senses " + //
 			"INNER JOIN synsets ON synset2id = synsetid " + //
 			"LEFT JOIN words ON word2id = wordid " + //
 			"LEFT JOIN relations USING (relationid) " + //
