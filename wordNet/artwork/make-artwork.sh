ea45116e3131ab40556730f90ca5a8385decefae
1313ou <1313ou@gmail.com>
2023-09-25 10:08:43 +0200

    new resources

--- wordNet/artwork/make-artwork.sh
+++ wordNet/artwork/make-artwork.sh
@@ -4,8 +4,10 @@ source "./lib-artwork.sh"
 
 icon_list="morph.svg adjposition.svg verbtemplate.svg verbframe.svg"
 asset_list=""
+settings_list="ic_settings_wordnet.svg"
 
 make_res "${icon_list}" 16
+make_res "${settings_list}" 24
 
 #rm ${dirassets}/images/wordnet/*
 make_icon "${asset_list}" 16 "${dirassets}/images/wordnet"
