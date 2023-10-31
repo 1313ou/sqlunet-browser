47ed9b33d584e6172fbe58aae9af6186e3047ebc
1313ou <1313ou@gmail.com>
2023-10-31 10:07:11 +0100

    remove some logging

--- browsersn/src/main/java/org/sqlunet/browser/sn/Application.java
+++ browsersn/src/main/java/org/sqlunet/browser/sn/Application.java
@@ -28,7 +28,7 @@ public class Application extends AbstractApplication
 	@Override
 	public void setAllColorsFromResources(@NonNull final Context context)
 	{
-		Log.d(TAG, "setColors " + NightMode.nightModeToString(this));
+		// Log.d(TAG, "setColors " + NightMode.nightModeToString(this));
 		Colors.setColorsFromResources(context);
 		org.sqlunet.syntagnet.style.Colors.setColorsFromResources(context);
 		org.sqlunet.wordnet.style.Colors.setColorsFromResources(context);
