27eefacae6b4634874cd846ccccc90662982e6c3
1313ou <1313ou@gmail.com>
2023-11-08 12:44:46 +0100

    house keeping

--- make-aabs.sh
+++ make-aabs.sh
@@ -4,29 +4,34 @@
 # Copyright (c) 2023. Bernard Bou
 #
 
-ENV="JAVA_HOME=/opt/java/java17 "
+#$1==:browser: :browserfn: :browservn: :browserwn: :browserewn: :browsersn: or null
 
+function rungradle()
+{
+export JAVA_HOME=/opt/java/java17
+./gradlew $@
+}
+
+# start
 case "$1" in
--clean)
-	echo "clean start"
-	${ENV}./gradlew --stop
-	${ENV}./gradlew clean
+-z)
+	shift
+	echo "stop daemon"
+	rungradle --stop
+	echo "clean task"
+	rungradle clean
 	;;
 *)
-	echo "resuming"
+	echo "using current daemon"
 	;;
 esac
 
-if ${ENV}./gradlew bundleRelease; then
+# determine which project
+which=$1
+echo "project=${which}"
+if rungradle ${which}bundleRelease; then
 	echo "success"
 else
 	echo "failed"
 fi
-exit
 
-#specific targets
-apps="browser browserfn browservn browserwn browserewn browsersn"
-for a in $apps; do
-	echo $a
-	${ENV}./gradlew :${a}:bundleRelease
-done
