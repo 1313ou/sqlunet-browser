44dbd5e03b0182ba91e9115a67588258ff3d59f5
1313ou <1313ou@gmail.com>
2023-11-14 17:57:41 +0100

    snackbar with icon

--- browserwn/src/main/java/org/sqlunet/browser/wn/Oewn.java
+++ browserwn/src/main/java/org/sqlunet/browser/wn/Oewn.java
@@ -7,7 +7,10 @@ package org.sqlunet.browser.wn;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.text.SpannableStringBuilder;
+import android.text.style.ImageSpan;
 import android.view.View;
+import android.widget.TextView;
 
 import com.bbou.others.OthersActivity;
 import com.google.android.material.behavior.SwipeDismissBehavior;
@@ -30,13 +33,13 @@ public class Oewn
 	{
 		final SharedPreferences prefs = activity.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE);
 		int launchTimes = prefs.getInt(PREF_KEY_LAUNCH_TIMES, 0);
-		if (launchTimes % HOW_OFTEN == 1)
+		//if (launchTimes % HOW_OFTEN == 1)
 		{
 			final SharedPreferences prefs2 = PreferenceManager.getDefaultSharedPreferences(activity);
 			boolean noNotice = prefs2.getBoolean(PREF_KEY_OEWN_NO_NOTICE, false);
-			if (!noNotice)
+			//if (!noNotice)
 			{
-				suggestOewn(activity);
+				suggestOewn1(activity);
 			}
 		}
 		prefs.edit().putInt(PREF_KEY_LAUNCH_TIMES, launchTimes + 1).apply();
@@ -57,4 +60,48 @@ public class Oewn
 					.setBehavior(behavior).show();
 		}
 	}
+
+	private static void suggestOewn1(@NonNull final Activity activity)
+	{
+		final BaseTransientBottomBar.Behavior behavior = new BaseTransientBottomBar.Behavior();
+		behavior.setSwipeDirection(SwipeDismissBehavior.SWIPE_DIRECTION_ANY);
+		final View parentLayout = activity.findViewById(R.id.activity_main_sub); // view to find a parent from
+		if (parentLayout != null)
+		{
+			String message1 = activity.getString(R.string.obsolete_app);
+			String message2 = activity.getString(R.string.new_app);
+			SpannableStringBuilder sb = new SpannableStringBuilder();
+			sb.append(message1).append('\n').append('@');
+			sb.setSpan(new ImageSpan(activity, R.drawable.logo_semantikos_ewn), sb.length() - 1, sb.length(), 0);
+			sb.append('\n').append(message2);
+
+			final Snackbar snackbar = Snackbar.make(parentLayout, sb, Snackbar.LENGTH_INDEFINITE);
+			snackbar.setTextMaxLines(10) //
+					.setBackgroundTint(ContextCompat.getColor(activity, R.color.snackbar_oewn)) //
+					.setAction(R.string.obsolete_get_oewn, view -> OthersActivity.install(activity.getString(R.string.semantikos_ewn_uri), activity)) //
+					.setActionTextColor(ContextCompat.getColor(activity, android.R.color.white)) //
+					.setBehavior(behavior).show();
+		}
+	}
+
+	private static void suggestOewn2(@NonNull final Activity activity)
+	{
+		final BaseTransientBottomBar.Behavior behavior = new BaseTransientBottomBar.Behavior();
+		behavior.setSwipeDirection(SwipeDismissBehavior.SWIPE_DIRECTION_ANY);
+		final View parentLayout = activity.findViewById(R.id.activity_main_sub); // view to find a parent from
+		if (parentLayout != null)
+		{
+			final Snackbar snackbar = Snackbar.make(parentLayout, R.string.obsolete_app, Snackbar.LENGTH_INDEFINITE);
+			snackbar.setTextMaxLines(10) //
+					.setBackgroundTint(ContextCompat.getColor(activity, R.color.snackbar_oewn)) //
+					.setAction(R.string.obsolete_get_oewn, view -> OthersActivity.install(activity.getString(R.string.semantikos_ewn_uri), activity)) //
+					.setActionTextColor(ContextCompat.getColor(activity, android.R.color.white)) //
+					.setBehavior(behavior);
+			View snackbarLayout = snackbar.getView();
+			TextView textView = (TextView) snackbarLayout.findViewById(com.google.android.material.R.id.snackbar_text);
+			textView.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.logo_semantikos_ewn, 0, 0);
+			snackbar.show();
+		}
+	}
+
 }
