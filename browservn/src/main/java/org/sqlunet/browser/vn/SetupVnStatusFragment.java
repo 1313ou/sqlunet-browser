413d6c336a50d306fa5fa7a3f23dfbda2f9368a8
1313ou <1313ou@gmail.com>
2024-03-19 18:30:12 +0100

    glue settings to imported modules

--- browservn/src/main/java/org/sqlunet/browser/vn/SetupVnStatusFragment.java
+++ browservn/src/main/java/org/sqlunet/browser/vn/SetupVnStatusFragment.java
@@ -92,8 +92,8 @@ public class SetupVnStatusFragment extends org.sqlunet.browser.config.SetupStatu
 			final Activity activity = requireActivity();
 			final String database = StorageSettings.getDatabasePath(activity);
 			final String free = StorageUtils.getFree(activity, database);
-			final com.bbou.download.Settings.Mode mode = com.bbou.download.Settings.Mode.getModePref(activity);
-			final String source = StorageSettings.getDbDownloadSourcePath(activity, mode == com.bbou.download.Settings.Mode.DOWNLOAD_ZIP_THEN_UNZIP || mode == com.bbou.download.Settings.Mode.DOWNLOAD_ZIP);
+			final com.bbou.download.preference.Settings.Mode mode = com.bbou.download.preference.Settings.Mode.getModePref(activity);
+			final String source = StorageSettings.getDbDownloadSourcePath(activity, mode == com.bbou.download.preference.Settings.Mode.DOWNLOAD_ZIP_THEN_UNZIP || mode == com.bbou.download.preference.Settings.Mode.DOWNLOAD_ZIP);
 			final int status = Status.status(activity);
 			final boolean existsDb = (status & Status.EXISTS) != 0;
 			final boolean existsTables = (status & Status.EXISTS_TABLES) != 0;
