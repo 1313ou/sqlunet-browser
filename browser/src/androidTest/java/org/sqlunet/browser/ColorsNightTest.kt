b45f4b301ee9cc80f86e58b55fc5bfa1ab6fb61e
1313ou <1313ou@gmail.com>
2024-03-25 08:11:53 +0100

    xn to kotlin

--- browser/src/androidTest/java/org/sqlunet/browser/ColorsNightTest.kt
+++ browser/src/androidTest/java/org/sqlunet/browser/ColorsNightTest.kt
@@ -1,22 +1,16 @@
 /*
  * Copyright (c) 2023. Bernard Bou
  */
+package org.sqlunet.browser
 
-package org.sqlunet.browser;
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.junit.runner.RunWith
 
-import org.junit.runner.RunWith;
-
-import androidx.appcompat.app.AppCompatDelegate;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.LargeTest;
-
-@RunWith(AndroidJUnit4.class)
+@RunWith(AndroidJUnit4::class)
 @LargeTest
-public class ColorsNightTest extends AbstractColors
-{
-	@Override
-	public int getMode()
-	{
-		return AppCompatDelegate.MODE_NIGHT_YES;
-	}
+class ColorsNightTest : AbstractColors() {
+    override val mode: Int
+        get() = AppCompatDelegate.MODE_NIGHT_YES
 }
