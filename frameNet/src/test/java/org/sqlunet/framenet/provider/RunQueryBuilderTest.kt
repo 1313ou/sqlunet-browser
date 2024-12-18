ccb5ce461085771c3a81206ebe79997c1cfd7f27
1313ou <1313ou@gmail.com>
2024-12-18 09:00:01 +0100

    add build query tests

---
+++ frameNet/src/test/java/org/sqlunet/framenet/provider/RunQueryBuilderTest.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2023. Bernard Bou
+ */
+package org.sqlunet.framenet.provider
+
+import org.junit.Test
+import org.querybuilder.Generator
+
+class RunQueryBuilderTest {
+
+    val module = "framenet"
+    val dirProto = "src/proto/${module}"
+
+    @Test
+    fun queryInstantiateSqliteDialect() {
+        // op, inDir, source, outDir, dest
+        Generator.main(arrayOf("instantiate", dirProto, "SqLiteDialect.java", "-"))
+    }
+
+    @Test
+    fun queryQ() {
+        // op, inDir, source, outDir, dest
+        Generator.main(arrayOf("generate_q_class", dirProto, "-", "Q", "org.sqlunet.${module}"))
+    }
+
+    @Test
+    fun queryV() {
+        // op, inDir, source, outDir, dest
+        Generator.main(arrayOf("generate_v_class", dirProto, "-", "V", "org.sqlunet.${module}"))
+    }
+
+    @Test
+    fun queryQV() {
+        // op, inDir, source, outDir, dest
+        Generator.main(arrayOf("generate_qv_class", dirProto, "-", "QV", "org.sqlunet.${module}"))
+    }
+
+
+    @Test
+    fun export() {
+        // op, inDir, outDir, dest
+        Generator.main(arrayOf("export", dirProto, "-"))
+    }
+}
