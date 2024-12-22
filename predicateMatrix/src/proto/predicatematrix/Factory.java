3147373662ea20c12b416a9eff07c969de74eaeb
1313ou <1313ou@gmail.com>
2024-12-22 08:10:37 +0100

    qualify pms columns to avoid ambiguity

--- predicateMatrix/src/proto/predicatematrix/Factory.java
+++ predicateMatrix/src/proto/predicatematrix/Factory.java
@@ -39,21 +39,21 @@ public class Factory implements Function<String, String[]>, Supplier<String[]>
 				break;
 
 			case PM_X:
-				table = "${pms.table} " + //
+				table = "${pms.table} AS ${as_pm} " + //
 						"LEFT JOIN ${roles.table} AS ${as_pmroles} USING (${roles.roleid}) " + //
 						"LEFT JOIN ${predicates.table} AS ${as_pmpredicates} USING (${predicates.predicateid}) " + //
 						"LEFT JOIN ${wnsynsets.table} USING (${wnsynsets.synsetid}) " + //
 
-						"LEFT JOIN ${vnclasses.table} AS ${as_vnclasses} ON ${pms.vn_classid} = ${as_vnclasses}.${vnclasses.vnclassid} " + //
-						"LEFT JOIN ${vnroles.table} AS ${as_vnroles} ON ${pms.vn_roleid} = ${as_vnroles}.${vnroles.vnroleid} " + //
+						"LEFT JOIN ${vnclasses.table} AS ${as_vnclasses} ON ${as_pm}.${pms.vn_classid} = ${as_vnclasses}.${vnclasses.vnclassid} " + //
+						"LEFT JOIN ${vnroles.table} AS ${as_vnroles} ON ${as_pm}.${pms.vn_roleid} = ${as_vnroles}.${vnroles.vnroleid} " + //
 						"LEFT JOIN ${vnroletypes.table} AS ${as_vnroletypes} ON ${as_vnroles}.${vnroletypes.vnroletypeid} = ${as_vnroletypes}.${vnroletypes.vnroletypeid} " + //
 
-						"LEFT JOIN ${pbrolesets.table} AS ${as_pbrolesets} ON ${pms.pb_rolesetid} = ${as_pbrolesets}.${pbrolesets.pbrolesetid} " + //
-						"LEFT JOIN ${pbroles.table} AS ${as_pbroles} ON ${pms.pb_roleid} = ${as_pbroles}.${pbroles.pbroleid} " + //
+						"LEFT JOIN ${pbrolesets.table} AS ${as_pbrolesets} ON ${as_pm}.${pms.pb_rolesetid} = ${as_pbrolesets}.${pbrolesets.pbrolesetid} " + //
+						"LEFT JOIN ${pbroles.table} AS ${as_pbroles} ON ${as_pm}.${pms.pb_roleid} = ${as_pbroles}.${pbroles.pbroleid} " + //
 						"LEFT JOIN ${pbargtypes.table} AS ${as_pbargs} ON ${as_pbroles}.${pbargtypes.pbargtypeid} = ${as_pbargs}.${pbargtypes.pbargtypeid} " + //
 
-						"LEFT JOIN ${fnframes.table} AS ${as_fnframes} ON ${pms.fn_frameid} = ${as_fnframes}.${fnframes.fnframeid} " + //
-						"LEFT JOIN ${fnfes.table} AS ${as_fnfes} ON ${pms.fn_feid} = ${as_fnfes}.${fnfes.fnfeid} " + //
+						"LEFT JOIN ${fnframes.table} AS ${as_fnframes} ON ${as_pm}.${pms.fn_frameid} = ${as_fnframes}.${fnframes.fnframeid} " + //
+						"LEFT JOIN ${fnfes.table} AS ${as_fnfes} ON ${as_pm}.${pms.fn_feid} = ${as_fnfes}.${fnfes.fnfeid} " + //
 						"LEFT JOIN ${fnfetypes.table} AS ${as_fnfetypes} ON ${as_fnfes}.${fnfetypes.fnfetypeid} = ${as_fnfetypes}.${fnfetypes.fnfetypeid}"
 						// + "LEFT JOIN ${fnlexunits.table} AS ${as_fnlus} ON ${pms.fn_luid} = ${as_fnlus}.${fnlexunits.fnluid}"
 				;
