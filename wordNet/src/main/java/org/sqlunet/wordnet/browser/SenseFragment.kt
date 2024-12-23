7cde544cfa594f6ed3dd6a42254b9df8eb4d7494
1313ou <1313ou@gmail.com>
2024-12-23 14:25:30 +0100

    parameters as properties

--- wordNet/src/main/java/org/sqlunet/wordnet/browser/SenseFragment.kt
+++ wordNet/src/main/java/org/sqlunet/wordnet/browser/SenseFragment.kt
@@ -4,7 +4,6 @@
 package org.sqlunet.wordnet.browser
 
 import org.sqlunet.browser.Module
-import org.sqlunet.provider.ProviderArgs
 import org.sqlunet.wordnet.R
 import org.sqlunet.wordnet.loaders.SenseModule
 
@@ -21,10 +20,7 @@ class SenseFragment : SynsetFragment() {
 
     override fun makeModule(): Module {
         val module = SenseModule(this)
-        module.setMaxRecursionLevel(maxRecursion)
-        if (parameters != null) {
-            module.setDisplayRelationNames(parameters!!.getBoolean(ProviderArgs.ARG_RENDER_DISPLAY_SEM_RELATION_NAME_KEY, true), parameters!!.getBoolean(ProviderArgs.ARG_RENDER_DISPLAY_LEX_RELATION_NAME_KEY, true))
-        }
+        baseModuleSettings(module)
         return module
     }
 
