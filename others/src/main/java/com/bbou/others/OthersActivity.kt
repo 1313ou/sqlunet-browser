6ca3a4b8f03954d0310cf7c10ea5f48ead2676a7
1313ou <1313ou@gmail.com>
2026-02-21 14:34:10 +0100

    downcast to view

--- others/src/main/java/com/bbou/others/OthersActivity.kt
+++ others/src/main/java/com/bbou/others/OthersActivity.kt
@@ -7,7 +7,9 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.annotation.StringRes
 import androidx.appcompat.app.ActionBar
@@ -38,19 +40,19 @@ class OthersActivity : BaseActivity() {
         actionBar.displayOptions = ActionBar.DISPLAY_SHOW_HOME or ActionBar.DISPLAY_HOME_AS_UP or ActionBar.DISPLAY_SHOW_TITLE
 
         // menu listeners
-        findViewById<Button>(R.id.market_grammarscope_syntaxnet)?.setOnClickListener { install(R.string.grammarscope_syntaxnet_uri) }
-        findViewById<Button>(R.id.market_grammarscope_syntaxnet_premium)?.setOnClickListener { install(R.string.grammarscope_syntaxnet_premium_uri) }
-        findViewById<Button>(R.id.market_grammarscope_udpipe)?.setOnClickListener { install(R.string.grammarscope_udpipe_uri) }
-        findViewById<Button>(R.id.market_grammarscope_udpipe_premium)?.setOnClickListener { install(R.string.grammarscope_udpipe_premium_uri) }
-        findViewById<Button>(R.id.market_grammarscope_corenlp)?.setOnClickListener { install(R.string.grammarscope_corenlp_uri) }
-        findViewById<Button>(R.id.market_grammarscope_corenlp_premium)?.setOnClickListener { install(R.string.grammarscope_corenlp_premium_uri) }
-        findViewById<Button>(R.id.market_treebolic_wordnet)?.setOnClickListener { install(R.string.semantikos_uri) }
-        findViewById<Button>(R.id.market_semantikos)?.setOnClickListener { install(R.string.treebolic_wordnet_uri) }
-        findViewById<Button>(R.id.market_semantikos_wn)?.setOnClickListener { install(R.string.semantikos_wn_uri) }
-        findViewById<Button>(R.id.market_semantikos_ewn)?.setOnClickListener { install(R.string.semantikos_ewn_uri) }
-        findViewById<Button>(R.id.market_semantikos_vn)?.setOnClickListener { install(R.string.semantikos_vn_uri) }
-        findViewById<Button>(R.id.market_semantikos_fn)?.setOnClickListener { install(R.string.semantikos_fn_uri) }
-        findViewById<Button>(R.id.market_semantikos_sn)?.setOnClickListener { install(R.string.semantikos_sn_uri) }
+        findViewById<View>(R.id.market_grammarscope_syntaxnet)?.setOnClickListener { install(R.string.grammarscope_syntaxnet_uri) }
+        findViewById<View>(R.id.market_grammarscope_syntaxnet_premium)?.setOnClickListener { install(R.string.grammarscope_syntaxnet_premium_uri) }
+        findViewById<View>(R.id.market_grammarscope_udpipe)?.setOnClickListener { install(R.string.grammarscope_udpipe_uri) }
+        findViewById<View>(R.id.market_grammarscope_udpipe_premium)?.setOnClickListener { install(R.string.grammarscope_udpipe_premium_uri) }
+        findViewById<View>(R.id.market_grammarscope_corenlp)?.setOnClickListener { install(R.string.grammarscope_corenlp_uri) }
+        findViewById<View>(R.id.market_grammarscope_corenlp_premium)?.setOnClickListener { install(R.string.grammarscope_corenlp_premium_uri) }
+        findViewById<View>(R.id.market_treebolic_wordnet)?.setOnClickListener { install(R.string.semantikos_uri) }
+        findViewById<View>(R.id.market_semantikos)?.setOnClickListener { install(R.string.treebolic_wordnet_uri) }
+        findViewById<View>(R.id.market_semantikos_wn)?.setOnClickListener { install(R.string.semantikos_wn_uri) }
+        findViewById<View>(R.id.market_semantikos_ewn)?.setOnClickListener { install(R.string.semantikos_ewn_uri) }
+        findViewById<View>(R.id.market_semantikos_vn)?.setOnClickListener { install(R.string.semantikos_vn_uri) }
+        findViewById<View>(R.id.market_semantikos_fn)?.setOnClickListener { install(R.string.semantikos_fn_uri) }
+        findViewById<View>(R.id.market_semantikos_sn)?.setOnClickListener { install(R.string.semantikos_sn_uri) }
     }
 
     private fun install(@StringRes uri: Int) {
